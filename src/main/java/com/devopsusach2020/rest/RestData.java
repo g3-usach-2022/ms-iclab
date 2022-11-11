55aaf31212bd066a20c221962fed2f0c7844ea3d
Francisca Olave Tapia <francisca.olave@coredevx.com>
2022-11-11 19:53:24 -0300

    Variables import java

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -1,6 +1,31 @@
+package com.devopsusach2020.rest;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.devopsusach2020.model.Pais;
+import com.devopsusach2020.model.Mundial;
+import com.google.gson.Gson;
+
+@RestController
+@RequestMapping(path = "/rest/mscovid")
+public class RestData {
+	
+	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
+
+	
 @GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE) 
-public @ResponseBody Pais getTotalPais(@ReguestParam(name = "pais") String message)
-        { RestTemplate restTemplate = new RestTemplate(); 
+public @ResponseBody Pais getTotalPais(@ReguestParam(name = "pais") String message){ 
+        RestTemplate restTemplate = new RestTemplate(); 
     ResponseEntity<String> call= restTemplate.getForEntity("https://api.covidl9api.com/live/country/" + message ,String.class); 
 
 LOGGER.log(Level.INFO, "Consulta por pais"); 
@@ -27,4 +52,5 @@ response. setCountry(message);
 response.setMensaje("ok"); 
 
         return response; 
-} 
+        } 
+}
