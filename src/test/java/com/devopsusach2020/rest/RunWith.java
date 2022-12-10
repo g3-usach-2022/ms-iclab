6e3aeaffda3d514f1e40d8bacd076deba97dff37
Bfal10 <31557753+Bfal10@users.noreply.github.com>
2022-12-10 11:15:22 -0300

    add api calls

---
+++ src/test/java/com/devopsusach2020/rest/RunWith.java
@@ -0,0 +1,10 @@
+package com.devopsusach2020.rest;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public @interface RunWith {
+
+    Class<SpringRunner> value();
+
+}
